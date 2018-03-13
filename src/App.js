da426c2054e99309ccf13b0d996d63a212c1d3f6
Tigran Petrossian <tgpett@gmail.com>
2018-03-13 18:16:21 +0400

    Add navigation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ const Header = (props) => {
   return (
     <header className="header">
       <h1 className="header__logo">HN</h1>
+      
+      <nav className="nav">
+        <a className="nav-link is-active" href="">Top</a>
+        <a className="nav-link" href="">New</a>
+        <a className="nav-link" href="">Show</a>
+        <a className="nav-link" href="">Ask</a>
+        <a className="nav-link" href="">Jobs</a>
+      </nav>
     </header>
   );
 };

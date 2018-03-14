33d08f5d662e6dba122b014d9aed72715b18ce66
Tigran Petrossian <tgpett@gmail.com>
2018-03-14 06:21:42 +0400

    Add EntryList

---
+++ src/components/EntryList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Entry from './Entry';
+
+const EntryList = (props) => {
+  return (
+    <div className="entry-list">
+      <Entry />
+    </div>
+  );
+};
+
+export default EntryList;

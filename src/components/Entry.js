814546b235a95b4bcb701fdf4f1c3fd584f2ddab
Tigran Petrossian <tgpett@gmail.com>
2018-03-15 01:26:44 +0400

    Make basic entry rendering with some dummy data

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,30 +1,40 @@
 import React from 'react';
-import * as time from '../utils/utils.time';
+import * as timeUtils from '../utils/utils.time';
 import { getExactTime } from '../utils/utils.time';
 import commentIcon from '../images/icon-comment.svg'
+import getHostName from '../utils/getHostname';
+import { getTimePassed } from '../utils/utils.time';
 
-const Entry = (props) => {
+const Entry = ({by, score, time, title, url, descendants, text}) => {
+  // type: story
+  // type: job ( just title and date)
+  // type: poll (maybe)
+  
+  
+  // When the entry doesn't have `url`, but has `text` instead (ask, show, job)
+  // url should point to the entry in that case, not to outside link.
+ 
   return (
     <div className="entry">
       <header className="entry__title">
-        <a className="entry__link" href="">
-          Show HN: RoughJS – Create hand-drawn graphics using JavaScript
+        <a className="entry__link" href={url}>
+          {title}
         </a>
         
-        <small className="entry__hostname">(roughjs.com)</small>
+        <small className="entry__hostname">({getHostName(url)})</small> {/* Don't render if show/ask */}
       </header>
       
       <div className="entry__meta">
-        <span className="entry__score">+ 88</span>
+        <span className="entry__score">+ {score}</span>
   
         <span className="entry__user">
-          by <a className="entry__user-link" href="">fallingmeat</a>
+          by <a className="entry__user-link" href={`/${by}`}>{by}</a>
         </span>
         
-        <time className="entry__time" dateTime={time.getISOTime(1520884294)} title={getExactTime(1520884294)}>2 hours ago</time>
+        <time className="entry__time" dateTime={timeUtils.getISOTime(time)} title={getExactTime(time)}>{getTimePassed(time)}</time>
         
         <a className="entry__comments-link" href="" aria-label="32 comments">
-          32
+          {descendants}
         </a>
       </div>
     </div>
--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Entry from './Entry';
+import mockData from '../mock';
 
 const EntryList = (props) => {
+  const entries = mockData.map(entryData => {
+    return (
+      <Entry key={entryData.id} {...entryData} />
+    );
+  });
+  
   return (
     <div className="entry-list">
-      <Entry />
+      {entries}
     </div>
   );
 };

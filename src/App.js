bff4e66aee8f775c8e7f7be421be2c19106e7f12
Tigran Petrossian <tgpett@gmail.com>
2018-03-14 06:21:28 +0400

    Move header to a separate component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React, { Component, Fragment } from 'react';
-
-const Header = (props) => {
-  return (
-    <header className="header">
-      <h1 className="header__logo">HN</h1>
-      
-      <nav className="nav">
-        <a className="nav-link is-active" href="">Top</a>
-        <a className="nav-link" href="">New</a>
-        <a className="nav-link" href="">Show</a>
-        <a className="nav-link" href="">Ask</a>
-        <a className="nav-link" href="">Jobs</a>
-      </nav>
-    </header>
-  );
-};
-
+import Header from './components/Header';
+import Main from './components/Main';
+import EntryList from './components/EntryList';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Header/>
+        <Main>
+          <EntryList/>
+        </Main>
       </Fragment>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <header className="header">
+      <h1 className="header__logo">HN</h1>
+      
+      <nav className="nav">
+        <a className="nav-link is-active" href="">Top</a>
+        <a className="nav-link" href="">New</a>
+        <a className="nav-link" href="">Show</a>
+        <a className="nav-link" href="">Ask</a>
+        <a className="nav-link" href="">Jobs</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

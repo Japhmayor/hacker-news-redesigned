6782f6b3cba776b178ecea0b6a2fb34fb34d337a
Tigran Petrossian <tgpett@gmail.com>
2018-03-16 01:29:01 +0400

    Add hover/active states to main and user links

--- src/components/Entry/EntryLink.js
+++ src/components/Entry/EntryLink.js
@@ -4,6 +4,15 @@ import { colors } from '../../styles/settings/colors';
 const EntryLink = styled.a`
   color: ${colors.neutral['10']};
   text-decoration: none;
+  transition: color 0.1s ease-in-out;
+  
+  &:hover {
+    color: ${colors.neutral['20']};
+  }
+  
+  &:active {
+    color: ${colors.neutral['30']};
+  }
 `;
 
 export default EntryLink;
--- src/components/Entry/EntryUserLink.js
+++ src/components/Entry/EntryUserLink.js
@@ -3,6 +3,15 @@ import { colors } from '../../styles/settings/colors';
 
 const EntryUserLink = styled.a`
   color: ${colors.neutral['30']};
+  transition: color 0.1s ease-in-out;
+  
+  &:hover {
+    color: ${colors.neutral['40']};
+  }
+  
+  &:active {
+    color: ${colors.neutral['50']};
+  }
 `;
 
 export default EntryUserLink;

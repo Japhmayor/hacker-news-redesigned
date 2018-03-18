8fe6347e41c0d226e40b35a0551371000de0719d
Tigran Petrossian <tgpett@gmail.com>
2018-03-19 02:45:04 +0400

    Temporarily reduce the container width

--- src/components/Header/HeaderWrap.js
+++ src/components/Header/HeaderWrap.js
@@ -1,23 +1,16 @@
 import styled from 'styled-components';
 import { colors, white } from '../../styles/settings/colors';
 import container from '../../styles/mixins/container';
-import { spacing } from '../../styles/settings/spacing';
-import { breakpoints } from '../../styles/settings/breakpoints';
 import { headingFont } from '../../styles/settings/typography';
 
 const HeaderWrap = styled.header`
   ${container};
   display: flex;
   height: 64px;
-  margin-bottom: ${spacing(8)};
   font-family: ${headingFont};
   color: ${white};
   background: ${colors.primary['50']};
   overflow: hidden;
-  
-  @media (min-width: ${breakpoints.md}) {
-      margin-bottom: ${spacing(10)};
-  }
 `;
 
 export default HeaderWrap;

acb385e9f8efa5fc96c93932146245a486844258
Tigran Petrossian <tgpett@gmail.com>
2018-03-23 01:16:12 +0400

    Add main feed routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header/';
 import Main from './components/Main';
 import EntryList from './components/EntryList/';
-import Post from './components/Post/Post';
-
-import postMock from './postMock';
+import EntryListContainer from './containers/EntryListContainer';
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Header />
-        <Main>
-          {/*<EntryList/>*/}
-          <Post {...postMock} />
-        </Main>
-      </Fragment>
+      <Router>
+        <Fragment>
+          <Header />
+          <Main>
+            <Route path="/" component={EntryListContainer} exact/>
+            <Route path="/:feed(top|new|ask|show|jobs|best)/:page([1-9][0-9]?)?" component={EntryListContainer} exact/>
+            {/*<EntryListContainer feedType="new" page="1"/>*/}
+          </Main>
+        </Fragment>
+      </Router>
     );
   }
 }

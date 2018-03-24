6fe68c2fb03c97fcbb93e8b89edbde7b31e41296
Tigran Petrossian <tgpett@gmail.com>
2018-03-24 14:31:53 +0400

    Simply EntryWrapper styling

--- src/components/Entry/EntryWrapper.js
+++ src/components/Entry/EntryWrapper.js
@@ -3,11 +3,9 @@ import { spacing } from '../../styles/settings/spacing';
 import { colors } from '../../styles/settings/colors';
 
 const EntryWrapper = styled.div`
-  & + & {
-    margin-top: ${spacing(5)};
-    padding-top: ${spacing(5)};
-    border-top: 1px solid ${colors.neutral['80']};
-  }
+  margin-bottom: ${spacing(5)};
+  padding-bottom: ${spacing(5)};
+  border-bottom: 1px solid ${colors.neutral['80']};
 `;
 
 export default EntryWrapper;

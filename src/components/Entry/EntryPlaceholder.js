7e9b5e61f18d8a482221cb9223235540f2a165c1
Tigran Petrossian <tgpett@gmail.com>
2018-03-29 19:01:29 +0400

    Use functional components for placeholders

--- src/components/Entry/EntryPlaceholder.js
+++ src/components/Entry/EntryPlaceholder.js
@@ -3,20 +3,14 @@ import entryPlaceholderImage from '../../images/entry-placeholder.svg';
 
 // TODO: The placeholder is ugly and generic. Need something that works & looks better.
 
-class EntryPlaceholder extends React.Component {
-  state = {};
-  
-  render() {
-    return (
-      <div style={{marginBottom: 20}}>
-        <img
-          src={entryPlaceholderImage}
-          alt="Loading"
-          style={{width: '100%', height: 78}}
-        />
-      </div>
-    )
-  }
-}
+const EntryPlaceholder = (props) => (
+  <div style={{marginBottom: 20}}>
+    <img
+      src={entryPlaceholderImage}
+      alt="Loading"
+      style={{width: '100%', height: 78}}
+    />
+  </div>
+);
 
 export default EntryPlaceholder;
--- src/components/Post/PostPlaceholder.js
+++ src/components/Post/PostPlaceholder.js
@@ -3,22 +3,14 @@ import postPlaceholderImage from '../../images/post-placeholder.svg';
 
 // TODO: The placeholder is ugly and generic. Need something that works & looks better.
 
-class PostPlaceholder extends React.Component {
-  state = {};
-  
-  render() {
-    return (
-      <div>
-        <img
-          src={postPlaceholderImage}
-          alt="Loading"
-          style={{width: '100%', height: 78}}
-        />
-      </div>
-    )
-  }
-}
+const PostPlaceholder = (props) => (
+  <div role="region" aria-live="polite">
+    <img
+      src={postPlaceholderImage}
+      alt="Loading"
+      style={{width: '100%', height: 78}}
+    />
+  </div>
+);
 
 export default PostPlaceholder;
-
-// TODO: aria-live

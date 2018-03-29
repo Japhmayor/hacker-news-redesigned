b7b5d5d81cef63383d80e987651764060e1c6eaa
Tigran Petrossian <tgpett@gmail.com>
2018-03-29 22:05:11 +0400

    Increase comment empty state top spacing

--- src/components/CommentList/CommentsEmptyState.js
+++ src/components/CommentList/CommentsEmptyState.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { colors } from '../../styles/settings/colors';
+import { spacing } from '../../styles/settings/spacing';
 
 const CommentsEmptyState = styled.h2`
   text-align: center;
-  padding-top: 40px;
+  margin-top: ${spacing(20)};
+  padding-top: ${spacing(10)};
   border-top: 1px solid ${colors.neutral['80']};
 `;
 
--- src/styles/settings/spacing.js
+++ src/styles/settings/spacing.js
@@ -3,7 +3,7 @@ import { isInt } from '../../utils/utils.math';
 const spacingUnit = 4;
 
 export function spacing(index) {
-  if (!(isInt(index) && index > 0 && index <= 10)) {
+  if (!(isInt(index) && index > 0 && index <= 20)) {
     console.error('Spacing utility expects an integer between 1 and 10.');
     return;
   }

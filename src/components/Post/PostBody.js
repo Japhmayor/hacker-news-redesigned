7c94d0c71b6d83f5b9221a5fbd0127e065ad8a34
Tigran Petrossian <tgpett@gmail.com>
2018-03-30 19:45:51 +0400

    Add bottom spacing to Postbody

--- src/components/Post/PostBody.js
+++ src/components/Post/PostBody.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
+import { spacing } from '../../styles/settings/spacing';
 
 const PostBody = styled.div`
+  margin-bottom: ${spacing(10)};
+
   p {
     margin-top: 1em;
     margin-bottom: 0;

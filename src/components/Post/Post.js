a8afa21750b31f75896eba32c9bc830dc67e5fd7
Tigran Petrossian <tgpett@gmail.com>
2018-04-03 06:19:16 +0400

    Convert Post loading to GraphQL (W.I.P)

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -17,77 +17,71 @@ import CommentList from '../CommentList/CommentList';
 import CommentsEmptyState from '../CommentList/CommentsEmptyState';
 import PollContainer from '../../containers/PollContainer';
 
-const Post = ({ id, type, url, title, text, score, by: author, time, parts, descendants: commentCount, kids: comments }) => {
-  const isLink = typeof url !== 'undefined';
-  
+const Post = ({ id, type, url, title, text, score, author, time, parts, commentCount }) => {
+  const isLink = (url !== null);
   
   return (
     <Fragment>
       <article>
         <PostHeader>
-          
+
           <PostTitle>
             {isLink ? (
                 <Fragment>
                   <EntryLink title={title} href={url} external={isLink} />
-                  
+
                   <EntryHostname>({getHostName(url)})</EntryHostname>
                 </Fragment>)
-              
+
               : title}
           </PostTitle>
-          
+
           <Meta>
             {score &&
             <Score>+ {score}</Score>}
-            
+
             {author &&
             <Author>
               by <EntryUserLink href={`/user/${author}`}>{author}</EntryUserLink>
             </Author>
             }
-            
+
             <Time
               to={`/post/${id}`}
               time={time}
             />
           </Meta>
-        
+
         </PostHeader>
-        
+
         {text &&
         <PostBody dangerouslySetInnerHTML={{ __html: text }} />
         }
-        
+
         {type === 'poll' && parts && parts.length > 0 &&
         <PollContainer pollIDs={parts}/>
         }
       </article>
-      
-      {commentCount > 0
-        ? <CommentList commentIDs={comments} commentCount={commentCount} />
-        : <CommentsEmptyState>No one commented yet</CommentsEmptyState>
-      }
+
+      {/*{commentCount > 0*/}
+        {/*? <CommentList commentIDs={comments} commentCount={commentCount} />*/}
+        {/*: <CommentsEmptyState>No one commented yet</CommentsEmptyState>*/}
+      {/*}*/}
     </Fragment>
   );
 };
 
-
-// TODO: Change accordingly when switching to GraphQL
-//       id -> string
-//       by -> rename to author
-//       descendants -> rename to commentCount
 Post.propTypes = {
-  id: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   url: PropTypes.string,
   title: PropTypes.string.isRequired,
   text: PropTypes.string,
   score: PropTypes.number.isRequired,
-  by: PropTypes.string,
+  author: PropTypes.string,
   time: PropTypes.number.isRequired,
   parts: PropTypes.array,
-  descendants: PropTypes.number,
+  commentCount: PropTypes.number,
   kids: PropTypes.array,
 };
 
--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -1,71 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import Post from '../components/Post/Post';
 import PostPlaceholder from '../components/Post/PostPlaceholder';
-import { getEntry } from '../HNApi';
 
-class PostContainer extends React.Component {
-  static propTypes = {
-    match: PropTypes.shape({
-      params: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-      })
-    }).isRequired
-  };
-  
-  state = {
-    post: null,
-    loading: true,
-    error: false,
-  };
+const PostQuery = gql`
+  query PostQuery($id: ID!) {
+    post(id: $id) {
+      id
+      title
+      author
+      type
+      url
+      text
+      score
+      time
+      commentCount
+    }
+  }
+`;
 
-  componentDidMount() {
-    this.update();
-  }
+const PostContainer = (props) => {
+  const id = props.match.params.id;
   
-  componentDidUpdate(prevProps) {
-    const currentID = this.props.match.params.id;
-    const prevID = prevProps.match.params.id;
-    
-    if (prevID !== currentID) {
-      this.update();
-    }
-  }
-  
-  update = () => { // TODO: Not a great name for this function. Rethink.
-    this.setState({
-      loading: true
-    });
-    
-    const id = this.props.match.params.id;
-    
-    getEntry(id)
-      .then(post => {
-        this.setState({
-          loading: false,
-          post,
-        });
-      })
-      .catch(error => {
-        this.setState({ error: true });
-        console.error(error)
-      });
-  };
-  
-  render() {
-    if (this.state.loading) {
-      return <PostPlaceholder/>
-    }
-    
-    if (this.state.error) {
-      return 'Failed loading the post. Please try again';
-    }
-    
-    return (
-      <Post {...this.state.post} />
-    )
-  }
-}
+  return (
+    <Query query={PostQuery} variables={{ id }}>
+      {
+        ({ data, loading, error }) => {
+          if (loading) return <PostPlaceholder />;
+          if (error) return 'Failed loading the post. Please try again';
+          
+          return <Post {...data.post} />;
+        }
+      }
+    </Query>
+  );
+};
+
+PostContainer.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    })
+  }).isRequired
+};
 
 export default PostContainer;
 

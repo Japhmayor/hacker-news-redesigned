a41159b65515009a88752a6a6073f1ed08c03b23
Tigran Petrossian <tgpett@gmail.com>
2018-04-10 01:44:34 +0400

    Convert CommentPage to scss

--- src/components/CommentPage/CommentPage.js
+++ src/components/CommentPage/CommentPage.js
@@ -5,32 +5,14 @@ import { fontSizeSmall, fontWeightMedium, h3FontSize } from '../../styles-old/se
 import { colors } from '../../styles-old/settings/colors';
 import { spacing } from '../../styles-old/settings/spacing';
 import Comment from '../Comment/Comment';
-
-
-const CommentPageTitle = styled.h1` // TODO: Move
-  margin-bottom: ${spacing(8)};
-  padding-bottom: ${spacing(4)};
-  font-size: ${h3FontSize};
-  color: ${rgba(colors.neutral['10'], 0.8)};
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  border-bottom: 1px solid ${colors.neutral['90']};
-  
-  small {
-    font-size: ${fontSizeSmall};
-    font-weight: ${fontWeightMedium};
-    color: ${colors.neutral['40']};
-    margin-right: 1ch;
-  }
-`;
+import * as styles from './CommentPage.scss';
 
 const CommentPage = (props) => (
   <Fragment>
-    <CommentPageTitle> {/* TODO: Should be a link to the post.*/}
+    <h1 className={styles.CommentPageTitle}> {/* TODO: Should be a link to the post.*/}
       <small>Comment thread in:</small>
       {props.parentPostTitle}
-    </CommentPageTitle>
+    </h1>
 
     <Comment {...props} />
   </Fragment>

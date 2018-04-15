a4d5243a3b0d4779267081d0ef4a2c49a550943f
Tigran Petrossian <tgpett@gmail.com>
2018-04-15 04:28:00 +0400

    Replace apollo-boost with manual setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error';
 import App from './App';
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+const client = new ApolloClient({ // TODO: Move this to a separate file.
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: 'http://localhost:4000/graphql',
+      credentials: 'same-origin'
+    })
+  ]),
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
@@ -15,4 +34,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// TODO: Caching should be handled throughout the app, all requests are network-only atm
+// TODO: Caching should be handled throughout the app, all requests are network-only atm.

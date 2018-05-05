7530f362ba6543184bcf340805276f520dd1147a
Tigran Petrossian <tgpett@gmail.com>
2018-05-06 03:35:41 +0400

    Use a class component for CommentList

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -2,19 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Comment from '../Comment/Comment';
 
-const CommentList = ({ comments, onLoadMore }) => {
-  setTimeout(_ => {
-    onLoadMore();
-  }, 100);
+class CommentList extends React.Component {
+  componentDidMount() {
+    setTimeout(this.props.onLoadMore, 100);
+  }
 
-  return (
-    <div>
-      {comments.map((comment) =>
-        <Comment key={comment.id} {...comment} level={0}/>,
-      )}
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        {this.props.comments.map((comment) =>
+          <Comment key={comment.id} {...comment} level={0}/>,
+        )}
+      </div>
+    )
+  }
+}
+
 
 CommentList.propTypes = {
   comments: PropTypes.arrayOf(PropTypes.object),

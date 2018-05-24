a00f504aed4cd99c1936978f007563bbd5f5d2be
Tigran Petrossian <tgpett@gmail.com>
2018-05-25 02:37:23 +0400

    Add User page and Comment page titles

--- src/components/CommentPage/CommentPage.js
+++ src/components/CommentPage/CommentPage.js
@@ -2,10 +2,13 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Comment from '../Comment/Comment';
+import Head from '../Head';
 import * as styles from './CommentPage.scss';
 
 const CommentPage = (props) => (
   <Fragment>
+    <Head title={props.parentPostTitle} />
+
     <h1 className={styles.CommentPageTitle}>
       <small>Comment thread in:</small>
       <Link to={`/post/${props.parentPostID}`}>{props.parentPostTitle}</Link>
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { getDate } from '../../utils/utils.time';
+import Head from '../Head';
 import * as styles from './User.scss';
 
 const User = ({ username, karma, createdAt, about }) => (
   <div className={styles.User}>
+    <Head title={`User: ${username}`} />
+
     <header className={styles.UserHeader}>
       <h2>{username}</h2>
       <span className={styles.UserScore} title="Karma">{karma}</span>

d1500f7bc77f6cef65524adcb19b2b4beb6daf91
Tigran Petrossian <tgpett@gmail.com>
2018-05-25 17:48:19 +0400

    Add splash icon and theme-color

--- src/server/index.js
+++ src/server/index.js
@@ -49,7 +49,7 @@ const render = (manifest) => (req, res) => {
         <head>
           <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                  
+          <meta name="theme-color" content="#ed732c"/>
           ${helmet.meta.toString()}
           ${helmet.link.toString()}
       

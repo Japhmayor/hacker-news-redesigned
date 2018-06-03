9d2dd6f4ea5253d4045f1d8f4371443729e65a88
Tigran Petrossian <tgpett@gmail.com>
2018-06-03 21:06:29 +0400

    Add Skip to Content link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../images/hn-logo.svg';
 import * as styles from './Header.scss';
 
 const Header = (props) => (
-  <header className={styles.Header} role="banner">
-    <div className={styles.Logo}>
-      <img src={logo} alt="Hacker News" />
+  <Fragment>
+    <div className={styles.SkipToContent}>
+      <a className=" u-sr-only u-sr-only-focusable" href="#main">
+        Skip to main content
+      </a>
     </div>
 
-    <nav className={styles.Nav}>
-      <NavLink className={styles.NavLink} activeClassName={styles.isActive} to="/" exact>Top</NavLink>
-      <NavLink className={styles.NavLink} activeClassName={styles.isActive} to="/new">New</NavLink>
-      <NavLink className={styles.NavLink} activeClassName={styles.isActive} to="/show">Show</NavLink>
-      <NavLink className={styles.NavLink} activeClassName={styles.isActive} to="/ask">Ask</NavLink>
-      <NavLink className={styles.NavLink} activeClassName={styles.isActive} to="/jobs">Jobs</NavLink>
-    </nav>
-  </header>
+    <header className={styles.Header} role="banner">
+      <div className={styles.Logo}>
+        <img src={logo} alt="Hacker News" />
+      </div>
+
+      <nav className={styles.Nav}>
+        <NavLink className={styles.NavLink} activeClassName={styles.isActive} to="/" exact>Top</NavLink>
+        <NavLink className={styles.NavLink} activeClassName={styles.isActive} to="/new">New</NavLink>
+        <NavLink className={styles.NavLink} activeClassName={styles.isActive} to="/show">Show</NavLink>
+        <NavLink className={styles.NavLink} activeClassName={styles.isActive} to="/ask">Ask</NavLink>
+        <NavLink className={styles.NavLink} activeClassName={styles.isActive} to="/jobs">Jobs</NavLink>
+      </nav>
+    </header>
+  </Fragment>
 );
 
 export default Header;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { main } from  './Main.scss';
 
 const Main = (props) => (
-  <main className={main} role="main">
+  <main id="main" className={main} role="main">
     {props.children}
   </main>
 );

73afb0cf11becdd73c341db06f74225c78ddc205
Tigran Petrossian <tgpett@gmail.com>
2018-06-10 13:14:32 +0400

    Disable compression on express

--- start.prod.js
+++ start.prod.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const compression = require('compression');
 const bodyParser = require('body-parser');
 const paths = require('./webpack/paths');
 const manifest = {
@@ -10,7 +9,6 @@ const serverRender = require('./build/server/bundle.server.js').default;
 const PORT_NUMBER = process.env.PORT || 8080;
 
 const app = express();
-app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(paths.buildPath));
 app.use(express.static(paths.buildStaticPath));

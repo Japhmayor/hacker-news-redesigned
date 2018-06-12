45ebec32427722f24c33e5c706a762bcadd3576b
Tigran Petrossian <tgpett@gmail.com>
2018-06-12 17:12:44 +0400

    Exclude /service-worker.js from HTTP cache

--- start.prod.js
+++ start.prod.js
@@ -9,9 +9,20 @@ const serverRender = require('./build/server/bundle.server.js').default;
 const PORT_NUMBER = process.env.PORT || 8080;
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(paths.buildPath, { maxAge: 2592000000 /* 1 month */ }));
-app.use(express.static(paths.buildStaticPath, { maxAge: 2592000000 /* 1 month */ }));
+
+// Static assets
+// Set max-age for all static assets to 1 month.
+app.use(function (req, res, next) {
+  // Prevent /service-worker.js from being cached so updates can be scheduled when the file changes.
+  if (req.url !== '/service-worker.js') {
+    res.header('Cache-Control', 'public, max-age=2592000')
+  }
+  next();
+});
+app.use(express.static(paths.buildPath));
+app.use(express.static(paths.buildStaticPath));
 app.use(serverRender(manifest));
 
 app.listen(PORT_NUMBER, () => {
